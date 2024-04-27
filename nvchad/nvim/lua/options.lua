9196bbeaefb73267102899a0afe6bda7b0716407
ozudev <ozudev@gmail.com>
2024-04-27 13:37:30 +0300

    nvchad

--- nvchad/nvim/lua/options.lua
+++ nvchad/nvim/lua/options.lua
@@ -4,3 +4,22 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.number = true -- Print line number
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.expandtab = true
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.mouse = "a" -- Enable mouse mode
+
+vim.opt.scrolloff = 10 -- Lines of context
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+
+
+-- vim.opt.breakindent = true
