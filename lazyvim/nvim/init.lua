46d96cceaf42d768c13455a27abb13d806585fc2
Oguzhan <104281049+codeozdev@users.noreply.github.com>
2024-08-02 17:01:07 +0300

    Update init.lua
    markdown spellcheck kaldirma

--- lazyvim/nvim/init.lua
+++ lazyvim/nvim/init.lua
@@ -1,6 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
+-- markdown dosyalarda spellcheck kaldirmaya yarar
+
+
 local luasnip = require("luasnip")
 luasnip.filetype_extend("typescriptreact", { "html" })
 require("luasnip/loaders/from_vscode").lazy_load()
